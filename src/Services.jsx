eec2a3692bd4a41e8ac890af1add4399fb87087f
Benediktas <benediktas.trilikauskas@code.berlin>
2025-06-25 11:20:04 +0300

    service fixed

--- src/Services.jsx
+++ src/Services.jsx
@@ -5,13 +5,13 @@ function Services() {
         <div id="services" className="services-section">
             <div className="services-container">
                 <div className="services-header">
-                    <h2>Mūsų <span>Paslaugos</span></h2>
+                    <h2>Mūsų <span className="paslaugos">Paslaugos</span></h2>
                     <p>Profesionalios padangų priežiūros paslaugos Klaipėdoje</p>
                 </div>
                 
                 <div className="services-layout">
                     {/* Main Featured Service */}
-                    <div className="service-main">
+                    <div className="service-main featured">
                         <div className="service-image">
                             <img src="./src/assets/ChatGPT Image Jun 24, 2025, 08_07_56 PM.png" alt="Padangų montavimas" />
                         </div>
@@ -21,34 +21,30 @@ function Services() {
                         </div>
                     </div>
 
-                    {/* second service */}
-                    <div className="service-main">
-                        <div className="service-image">
-                            <img src="./src/assets/ChatGPT Image Jun 18, 2025, 01_42_07 PM.png" alt="Padangų montavimas" />
+                    {/* Secondary Services */}
+                    <div className="services-secondary">
+                        {/* Padangų Remontas */}
+                        <div className="service-main small">
+                            <div className="service-image">
+                                <img src="./src/assets/ChatGPT Image Jun 18, 2025, 01_42_07 PM.png" alt="Padangų remontas" />
+                            </div>
+                            <div className="service-content">
+                                <h3>Padangų Remontas</h3>
+                                <p>Greitas ir kokybiškas padangų taisymas. Užtikriname jūsų saugumą kelyje.</p>
+                            </div>
                         </div>
-                        <div className="service-content">
-                            <h3>Padangų Remontas</h3>
-                            <p>Greitas ir kokybiškas padangų taisymas. Užtikriname jūsų saugumą kelyje.</p>
+
+                        {/* Padangų Saugojimas */}
+                        <div className="service-main small">
+                            <div className="service-image">
+                                <img src="./src/assets/IMG_2626.JPG" alt="Padangų saugojimas" />
+                            </div>
+                            <div className="service-content">
+                                <h3>Padangų Saugojimas</h3>
+                                <p>Saugus padangų saugojimas mūsų sandėlyje. Apsauga nuo žalos ir ilgesnis padangų tarnavimo laikas.</p>
+                            </div>
                         </div>
                     </div>
-
-                    {/* third service */}
-                    <div className="service-main">
-                        <div className="service-image">
-                            <img src="./src/assets/IMG_2626.JPG" alt="Padangų montavimas" />
-                        </div>
-                        <div className="service-content">
-                            <h3>Padangų Saugojimas</h3>
-                            <p>Saugus padangų saugojimas mūsų sandėlyje. Apsauga nuo žalos ir ilgesnis padangų tarnavimo laikas.
-                            </p>
-                        </div>
-                    </div>
-
-                    
-
-
-            
-
                 </div>
             </div>
         </div>
